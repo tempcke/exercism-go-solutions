f634ac3d5adb4613560fbf8f7542144d1fec5e0d
tempcke <t.github@logikostech.net>
2020-01-06 13:29:08 -0600

    hamming solution #1

---
+++ hamming/hamming.go
@@ -0,0 +1,28 @@
+// Package hamming is used to calculate the Hamming Distance between two DNA strands.
+package hamming
+
+//InputError strings must be the same length
+type InputError string
+
+func (e InputError) Error() string {
+	return string(e)
+}
+
+//ErrLengthMismatch strings must be the same length
+const ErrLengthMismatch = InputError("strings must be the same length")
+
+//Distance Find the number of differences between 2 equal length strings
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, ErrLengthMismatch
+	}
+
+	d := 0
+	for i := 0; i < len(a); i++ {
+		if a[i:i+1] != b[i:i+1] {
+			d++
+		}
+	}
+
+	return d, nil
+}
