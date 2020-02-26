f9ee128184fec971960e595ccd470d2c0d400bd7
tempcke <t.github@logikostech.net>
2020-02-26 12:07:46 -0600

    exercisim go scrabble_score - followed mentor's suggestions to improve speed

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,32 +1,44 @@
 // Package scrabble deals with the scrabble game
 package scrabble
 
-import "strings"
+import (
+	"unicode"
+)
 
-var letterValues = map[string]int{
-	"aeioulnrst": 1,
-	"dg":         2,
-	"bcmp":       3,
-	"fhvwy":      4,
-	"k":          5,
-	"jx":         8,
-	"qz":         10,
+var runeValMap = map[rune]int{
+	'a': 1,
+	'b': 3,
+	'c': 3,
+	'd': 2,
+	'e': 1,
+	'f': 4,
+	'g': 2,
+	'h': 4,
+	'i': 1,
+	'j': 8,
+	'k': 5,
+	'l': 1,
+	'm': 3,
+	'n': 1,
+	'o': 1,
+	'p': 3,
+	'q': 10,
+	'r': 1,
+	's': 1,
+	't': 1,
+	'u': 1,
+	'v': 4,
+	'w': 4,
+	'x': 8,
+	'y': 4,
+	'z': 10,
 }
 
 // Score returns the word score or sum of letter values in the word
 func Score(word string) int {
 	s := 0
-	for _, letter := range strings.ToLower(word) {
-		s += letterValue(string(letter))
+	for _, letter := range word {
+		s += runeValMap[unicode.ToLower(letter)]
 	}
 	return s
-}
-
-func letterValue(letter string) int {
-	for pool, val := range letterValues {
-		if strings.Contains(pool, letter) {
-			return val
-		}
-	}
-	return 0
 }
