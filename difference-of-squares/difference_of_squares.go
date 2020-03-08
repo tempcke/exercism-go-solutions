417f41e6f0a6917635f69e3c98a52e3b7b24a0d4
tempcke <t.github@logikostech.net>
2020-03-07 21:07:20 -0600

    difference-of-squares solved, but not optimized

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,1 +1,24 @@
 package diffsquares
+
+// SquareOfSum sums the first n natural numbers then squares it
+func SquareOfSum(n int) int {
+	var sum int
+	for ; n > 0; n-- {
+		sum += n
+	}
+	return sum * sum
+}
+
+// SumOfSquares sums the square of the first n natural numbers
+func SumOfSquares(n int) int {
+	var sum int
+	for ; n > 0; n-- {
+		sum += n * n
+	}
+	return sum
+}
+
+// Difference subtracts the SquareOfSum from the SumOfSquares to find the difference
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
