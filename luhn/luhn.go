b9ae9d1a90b873a42f04451b912b26c7df3cb696
tempcke <t.github@logikostech.net>
2020-03-16 09:03:16 -0500

    luhn solution 2

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -4,35 +4,33 @@ package luhn
 func Valid(input string) bool {
 	runes := []rune(input)
 	length := len(runes)
-	var sum int32
-	var count int32
+	var sum, count int32
 
 	for i := 0; i < length; i++ {
 		r := runes[length-i-1]
-		v := r - '0'
 		switch {
 		default:
 			return false
 		case r == ' ':
 			continue
-		case isNum(v):
-			sum += modVal(v, count)
+		case isNum(r):
+			sum += modVal(r-'0', count)
 			count++
 		}
 	}
 	return count > 1 && sum%10 == 0
 }
 
-func isNum(v int32) bool {
-	return 0 <= v && v <= 9
+func isNum(r rune) bool {
+	return '0' <= r && r <= '9'
 }
 
-func modVal(v, count int32) int32 {
+func modVal(value, count int32) int32 {
 	if count%2 != 0 {
-		v *= 2
-		if v > 9 {
-			v -= 9
+		value *= 2
+		if value > 9 {
+			value -= 9
 		}
 	}
-	return v
+	return value
 }
