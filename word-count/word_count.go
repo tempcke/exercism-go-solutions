5b2d78d7ba9578b8b07a0d6e87d15f93939ee6dd
tempcke <t.github@logikostech.net>
2020-03-16 14:45:45 -0500

    word-count solution 2 learned strings.FieldsFunc, much cleaner now

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,60 +1,32 @@
 package wordcount
 
-import "unicode"
+import (
+	"strings"
+	"unicode"
+)
 
 // Frequency is a word count map
 type Frequency map[string]int
+
+func (f Frequency) incWord(word string) {
+	f[strings.Trim(word, "'")]++
+}
 
 // WordCount counts the words in the provided input
 func WordCount(input string) Frequency {
 	f := make(Frequency)
 
-	var word string
-
-	for i, char := range input {
-		if !isValidChar(input, i) {
-			if len(word) > 0 {
-				f[word]++
-				word = ""
-			}
-			continue
-		}
-		word += string(unicode.ToLower(char))
-	}
-
-	if len(word) > 0 {
-		f[word]++
+	for _, word := range getWords(input) {
+		f.incWord(word)
 	}
 
 	return f
 }
 
-func isValidChar(input string, index int) bool {
-	return isLetterOrNumber(rune(input[index])) || isApostrophe(input, index)
+func getWords(input string) []string {
+	return strings.FieldsFunc(strings.ToLower(input), isNotWordChar)
 }
 
-func isLetterOrNumber(r rune) bool {
-	return unicode.IsLetter(r) || unicode.IsDigit(r)
-}
-
-func isApostrophe(input string, index int) bool {
-	if input[index] != '\'' {
-		return false
-	}
-
-	// ensure ' is not the first or last char in string
-	if index == 0 || index == len(input)-1 {
-		return false
-	}
-
-	// must have a letter before and after it
-	if !isLetter(input[index-1]) || !isLetter(input[index+1]) {
-		return false
-	}
-
-	return true
-}
-
-func isLetter(char byte) bool {
-	return unicode.IsLetter(rune(char))
+func isNotWordChar(r rune) bool {
+	return !unicode.IsLetter(r) && !unicode.IsDigit(r) && r != '\''
 }
