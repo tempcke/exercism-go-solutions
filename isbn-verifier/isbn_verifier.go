88685a3d971dd04958341f989f93e00b3a32b039
tempcke <t.github@logikostech.net>
2020-03-18 22:02:59 -0500

    isbn-verifiers solution 2

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -5,29 +5,18 @@ func IsValidISBN(isbn string) bool {
 	var t, m int32
 	m = 10
 	for _, r := range isbn {
-		if v, ok := isbnCharToInt(r); ok {
-			if m < 1 { // to long
-				return false
-			}
-			if v == 10 && m != 1 { // X somewhere other than last position
-				return false
-			}
-			t += m * v
+		switch {
+		case m < 1: // to long
+			return false
+		case '0' <= r && r <= '9':
+			t += (r - '0') * m
 			m--
+		case r == 'X' && m == 1:
+			t += 10 * m
+			m--
+		case r != '-': // invalid chars
+			return false
 		}
 	}
-	if m > 0 { // to short
-		return false
-	}
-	return t%11 == 0
-}
-
-func isbnCharToInt(char rune) (v int32, ok bool) {
-	if '0' <= char && char <= '9' {
-		return char - '0', true
-	}
-	if char == 'X' {
-		return 10, true
-	}
-	return 0, false
+	return m == 0 && t%11 == 0
 }
