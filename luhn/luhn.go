af002271b47d3f70ca3c9dbe2db5f17f75132805
tempcke <t.github@logikostech.net>
2020-03-26 13:25:12 -0500

    luhn - mentor likes long functions.....

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -2,33 +2,30 @@ package luhn
 
 // Valid checks if the input string is valid luhn
 func Valid(input string) bool {
-	sum, count := luhnSum([]rune(input))
+	runes := []rune(input)
+	var sum, count int32
 
-	return count > 1 && sum%10 == 0
-}
-
-func luhnSum(runes []rune) (sum, count int32) {
 	for i := len(runes) - 1; i >= 0; i-- {
 		r := runes[i]
 		if r == ' ' {
 			continue
 		}
 		if r < '0' || r > '9' {
-			return 0, 0
+			return false
 		}
 
-		sum += modVal(r-'0', count)
+		value := r - '0'
+
+		if count%2 != 0 {
+			value *= 2
+			if value > 9 {
+				value -= 9
+			}
+		}
+
+		sum += value
 		count++
 	}
-	return sum, count
-}
 
-func modVal(value, count int32) int32 {
-	if count%2 != 0 {
-		value *= 2
-		if value > 9 {
-			value -= 9
-		}
-	}
-	return value
+	return count > 1 && sum%10 == 0
 }
