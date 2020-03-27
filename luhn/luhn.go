6ddd206dbb55b76c4122001708d86dd35404d16f
tempcke <t.github@logikostech.net>
2020-03-27 08:15:08 -0500

    luhn solution 7

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,31 +1,45 @@
 package luhn
 
+import "strings"
+
 // Valid checks if the input string is valid luhn
 func Valid(input string) bool {
-	runes := []rune(input)
-	var sum, count int32
+	input = strings.ReplaceAll(input, " ", "")
 
-	for i := len(runes) - 1; i >= 0; i-- {
-		r := runes[i]
-		if r == ' ' {
-			continue
-		}
+	if len(input) <= 1 {
+		return false
+	}
+
+	var sum int32
+	var change bool = len(input)%2 == 0
+
+	for _, r := range input {
 		if r < '0' || r > '9' {
 			return false
 		}
 
-		value := r - '0'
+		// v := r - '0'
 
-		if count%2 != 0 {
-			value *= 2
-			if value > 9 {
-				value -= 9
-			}
-		}
+		// if change {
+		// 	v *= 2
+		// 	if v > 9 {
+		// 		v -= 9
+		// 	}
+		// }
 
-		sum += value
-		count++
+		sum += getValue(r-'0', change)
+		change = !change
 	}
 
-	return count > 1 && sum%10 == 0
+	return sum%10 == 0
+}
+
+func getValue(v int32, change bool) int32 {
+	if change {
+		if v > 4 {
+			return 2*v - 9
+		}
+		return 2 * v
+	}
+	return v
 }
