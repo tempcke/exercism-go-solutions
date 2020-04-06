8e0bcc87d84ae040f2d933d0879986fdc3a64ce5
tempcke <t.github@logikostech.net>
2020-04-06 16:04:29 -0500

    parallel_letter_frequency - first idea, not passing

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,5 +1,10 @@
 package letter
 
+import (
+	"fmt"
+	"sync"
+)
+
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -11,4 +16,42 @@ func Frequency(s string) FreqMap {
 		m[r]++
 	}
 	return m
+}
+
+func runeCtr(c chan<- rune, s string) {
+	for _, r := range s {
+		c <- r
+	}
+}
+
+// ConcurrentFrequency counts the frequency of each rune in all strings
+// processing each string in parallel
+func ConcurrentFrequency(strings []string) FreqMap {
+
+	var wg sync.WaitGroup
+
+	c := make(chan rune)
+
+	m := FreqMap{}
+	go func() {
+		for r := range c {
+			m[r]++
+		}
+	}()
+
+	for _, s := range strings {
+		wg.Add(1)
+		go func(c chan rune) {
+			runeCtr(c, s)
+			wg.Done()
+		}(c)
+	}
+
+	wg.Wait()
+	close(c)
+
+	fmt.Printf("%v", m)
+
+	return m
+
 }
