1212d00dcb1345996313320aec631d3c2286c46c
tempcke <t.github@logikostech.net>
2020-04-07 16:54:44 -0500

    sieve - working but not optimized

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,1 +1,20 @@
 package sieve
+
+func Sieve(limit int) []int {
+	s := make([]bool, limit+1)
+	p := make([]int, 0, limit)
+
+	s[0], s[1] = true, true
+
+	for i := 2; i <= limit; i++ {
+		isPrime := !s[i]
+		if isPrime {
+			p = append(p, i)
+			for j := i; j <= int(limit/i); j++ {
+				notPrime := i * j
+				s[notPrime] = true
+			}
+		}
+	}
+	return p
+}
