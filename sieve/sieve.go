d2f8560b419aa3726ab083018e8aa980470f6b65
tempcke <t.github@logikostech.net>
2020-04-07 16:56:16 -0500

    sieve - removed unneeded vars

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -7,12 +7,10 @@ func Sieve(limit int) []int {
 	s[0], s[1] = true, true
 
 	for i := 2; i <= limit; i++ {
-		isPrime := !s[i]
-		if isPrime {
+		if !s[i] {
 			p = append(p, i)
 			for j := i; j <= int(limit/i); j++ {
-				notPrime := i * j
-				s[notPrime] = true
+				s[i*j] = true
 			}
 		}
 	}
