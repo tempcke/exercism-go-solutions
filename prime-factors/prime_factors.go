f800ac576159a701bc3222bf9ba2783ce494d1bb
tempcke <t.github@logikostech.net>
2020-04-08 12:46:26 -0500

    prime_factors - solved with sieve, but results in out of memory errors on very large primes such as 93819012551

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,1 +1,26 @@
 package prime
+
+// Factors computes the prime factors of a given natural number
+func Factors(num int64) []int64 {
+	marked := make([]bool, num+1)
+	factors := make([]int64, 0, 10)
+
+	for i := int64(2); i <= num; i++ {
+		// if i is not prime then skip it
+		if marked[i] {
+			continue
+		}
+
+		for num%i == 0 {
+			factors = append(factors, i)
+			num /= i
+		}
+
+		// mark multiples as not prime
+		for mult := i * 2; mult <= num; mult += i {
+			marked[mult] = true
+		}
+	}
+
+	return factors
+}
