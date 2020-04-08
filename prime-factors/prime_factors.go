70ba2e56ec40aaaaa95fd5c2c79d2a79aec80715
tempcke <t.github@logikostech.net>
2020-04-08 13:05:28 -0500

    prime_factors - all tests working with sieve

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,13 +1,19 @@
 package prime
 
+const sieveLimit = 1000
+
 // Factors computes the prime factors of a given natural number
 func Factors(num int64) []int64 {
-	marked := make([]bool, num+1)
+	limit := num
+	if num > sieveLimit {
+		limit = sieveLimit
+	}
+	marked := make([]bool, limit+1)
 	factors := make([]int64, 0, 10)
 
 	for i := int64(2); i <= num; i++ {
 		// if i is not prime then skip it
-		if marked[i] {
+		if i < limit && marked[i] {
 			continue
 		}
 
@@ -17,7 +23,7 @@ func Factors(num int64) []int64 {
 		}
 
 		// mark multiples as not prime
-		for mult := i * 2; mult <= num; mult += i {
+		for mult := i * 2; mult <= num && mult <= limit; mult += i {
 			marked[mult] = true
 		}
 	}
