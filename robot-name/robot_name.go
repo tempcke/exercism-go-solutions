731ae07c5ff803027b04f9561138616ab4ca7313
tempcke <t.github@logikostech.net>
2020-04-18 21:15:50 -0500

    robot_name solution 1

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -2,15 +2,12 @@ package robotname
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
 	"time"
 )
 
-const (
-	nameLetters   = 2
-	nameLength    = 5
-	possibleCount = 26 * 26 * 10 * 10 * 10
-)
+const possibleCount = 26 * 26 * 10 * 10 * 10
 
 var usedNames = map[string]bool{}
 
@@ -19,44 +16,42 @@ type Robot struct {
 	name string
 }
 
-// Reset the robots name
-func (r *Robot) Reset() {
-	r.name, _ = genName()
-}
-
 // Name of the robot
 func (r *Robot) Name() (string, error) {
 	if r.name == "" {
-		name, err := genName()
-		if err != nil {
-			return "", err
+		if len(usedNames) == possibleCount {
+			return "", errors.New("possible names exausted")
 		}
-		r.name = name
+		strName := randName()
+		for usedNames[strName] {
+			strName = randName()
+		}
+		usedNames[strName] = true
+		r.name = strName
 	}
 	return r.name, nil
 }
 
-func genName() (string, error) {
-	if len(usedNames) == possibleCount {
-		return "", errors.New("possible names exausted")
-	}
-	strName := randName()
-	for usedNames[strName] {
-		strName = randName()
-	}
-	usedNames[strName] = true
-	return strName, nil
+// Reset the robots name
+func (r *Robot) Reset() {
+	r.name = ""
 }
 
 func randName() string {
 	rand.Seed(time.Now().UnixNano())
-	name := make([]rune, nameLength)
-	i := 0
-	for ; i < nameLetters; i++ {
-		name[i] = 'A' + int32(rand.Intn(26))
-	}
-	for ; i < nameLength; i++ {
-		name[i] = '0' + int32(rand.Intn(10))
-	}
-	return string(name)
+	return fmt.Sprintf(
+		"%c%c%c%c%c",
+		randLetter(),
+		randLetter(),
+		randDigit(),
+		randDigit(),
+		randDigit(),
+	)
+}
+
+func randLetter() rune {
+	return 'A' + int32(rand.Intn(26))
+}
+func randDigit() rune {
+	return '0' + int32(rand.Intn(10))
 }
