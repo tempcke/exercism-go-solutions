a06ba7402e16b05544fcb66ea7f2cec71f106696
tempcke <t.github@logikostech.net>
2020-04-21 06:55:54 -0500

    series solved

--- series/series.go
+++ series/series.go
@@ -1,1 +1,15 @@
 package series
+
+// All returns a list of all substrings of s with length n
+func All(n int, s string) []string {
+	substrs := make([]string, 0, len(s))
+	for i := 0; i <= len(s)-n; i++ {
+		substrs = append(substrs, s[i:i+n])
+	}
+	return substrs
+}
+
+// UnsafeFirst returns the first substring of s with length n
+func UnsafeFirst(n int, s string) string {
+	return s[0:n]
+}
