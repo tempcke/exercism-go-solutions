00b9e93ec348e1cfb8a1ceadd244bab089462faf
tempcke <t.github@logikostech.net>
2020-04-21 06:56:11 -0500

    series solved with bonus

--- series/series.go
+++ series/series.go
@@ -13,3 +13,11 @@ func All(n int, s string) []string {
 func UnsafeFirst(n int, s string) string {
 	return s[0:n]
 }
+
+// First returns the first substring of s with length n and ok to indicate there wasn't a problem
+func First(n int, s string) (string, bool) {
+	if n > len(s) {
+		return "", false
+	}
+	return s[0:n], true
+}
