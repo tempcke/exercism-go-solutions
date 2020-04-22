beb2616bde6d51604b8b423ae5d3560efb617435
tempcke <t.github@logikostech.net>
2020-04-22 17:02:10 -0500

    phone_number cleanup and simplify, no faster though

--- phone-number/phone_number.go
+++ phone-number/phone_number.go
@@ -8,90 +8,55 @@ import (
 
 const format = "(%s) %s-%s"
 
-type phNum struct {
-	number string
-}
+// Number cleans up differently formated telephone numbers
+// result is just 10 digits.  Ex: 6139950253
+func Number(input string) (string, error) {
 
-func newPhNum(input string) (phNum, error) {
 	var num strings.Builder
-
 	for _, r := range input {
 		if '0' <= r && r <= '9' {
-			if num.Len() == 0 && r == '1' {
-				continue
-			}
 			num.WriteRune(r)
 		}
 	}
 
-	p := phNum{number: num.String()}
-	return p, p.validate()
-}
+	p := num.String()
 
-func (p phNum) validate() error {
+	if len(p) == 11 && p[0] == '1' {
+		p = p[1:]
+	}
+
 	switch {
-	case len(p.number) < 10 || len(p.number) > 11:
-		return errors.New("Number must be 10 or 11 digits long")
+	case len(p) != 10:
+		return "", errors.New("Number must be 10, or 11 if it starts with 1")
 
-	case len(p.number) == 11 && p.number[0] != '1':
-		return errors.New("invalid when 11 digits does not start with a 1")
+	case p[0] <= '1':
+		return "", errors.New("area code may not start with zero or one")
 
-	case p.area()[0] == '0':
-		return errors.New("area code may not start with zero")
-
-	case p.exchange()[0] == '0' || p.exchange()[0] == '1':
-		return errors.New("exchange code may not start with zero or one")
-	}
-	return nil
-}
-
-func (p phNum) area() string {
-	return p.number[0:3]
-}
-
-func (p phNum) exchange() string {
-	return p.number[3:6]
-}
-
-func (p phNum) subscriber() string {
-	return p.number[6:]
-}
-
-// Number cleans up differently formated telephone numbers
-// result is just 10 digits.  Ex: 6139950253
-func Number(input string) (string, error) {
-	p, err := newPhNum(input)
-
-	if err != nil {
-		return "", err
+	case p[3] <= '1':
+		return "", errors.New("exchange code may not start with zero or one")
 	}
 
-	return p.number, nil
+	return p, nil
 }
 
 // AreaCode returns the area code of a phone number
 func AreaCode(input string) (string, error) {
-	p, err := newPhNum(input)
+	p, err := Number(input)
 
 	if err != nil {
 		return "", err
 	}
 
-	return p.area(), nil
+	return p[0:3], nil
 }
 
 // Format phone number in the format of (613) 995-0253
 func Format(input string) (string, error) {
-	p, err := newPhNum(input)
+	p, err := Number(input)
 
 	if err != nil {
 		return "", err
 	}
 
-	return fmt.Sprintf(
-		format,
-		p.area(),
-		p.exchange(),
-		p.subscriber(),
-	), nil
+	return fmt.Sprintf(format, p[0:3], p[3:6], p[6:]), nil
 }
