d87eea0c8ff6a39afd52c77ffb4e32420e3b1759
tempcke <t.github@logikostech.net>
2020-09-21 15:32:54 -0500

    simple_cipher - remove need to retain key in vigenereCipher by simplifying Decode

--- simple-cipher/simple_cipher.go
+++ simple-cipher/simple_cipher.go
@@ -3,45 +3,44 @@ package cipher
 import "strings"
 
 // caesarCipher is a cipher style
-type caesarCipher struct {
-	offset int
+type shiftCipher struct {
+	distance int
 }
 
-// NewCaesar returns a caesar style cipher with a 4 char offset
+// NewCaesar returns a caesar style cipher with a 3 char shift
 func NewCaesar() Cipher {
-	return caesarCipher{offset: 3}
+	return shiftCipher{distance: 3}
 }
 
-// NewShift returns a cipher with a custom offset
+// NewShift returns a cipher with a custom shift
 func NewShift(distance int) Cipher {
 	if distance >= 26 || distance <= -26 || distance == 0 {
 		return nil
 	}
-	return caesarCipher{offset: distance}
+	return shiftCipher{distance: distance}
 }
 
-func (c caesarCipher) Encode(s string) string {
+func (c shiftCipher) Encode(s string) string {
 	runes := make([]rune, len(s))
-	i := 0
+	i := 0 // used to track only a-z chars
 	for _, r := range strings.ToLower(s) {
 		if r >= 'a' && r <= 'z' {
-			runes[i] = runeShift(r, c.offset)
+			runes[i] = runeShift(r, c.distance)
 			i++
 		}
 	}
 	return string(runes[0:i])
 }
 
-func (c caesarCipher) Decode(s string) string {
-	// a decode is just an ecode with a negative offset...
-	decoder := NewShift(-1 * c.offset)
+func (c shiftCipher) Decode(s string) string {
+	// a decode is just an ecode with a negative shift...
+	decoder := NewShift(-1 * c.distance)
 	return decoder.Encode(s)
 }
 
 // vigenereCipher is a cipher style
 type vigenereCipher struct {
-	key     string
-	offsets []int
+	distance []int
 }
 
 // NewVigenere defines a more complex cipher using a string as key value: a Vigenère cipher
@@ -49,44 +48,35 @@ func NewVigenere(key string) Cipher {
 	if strings.Count(key, "a") == len(key) {
 		return nil
 	}
-	offsets := make([]int, len(key))
+	distances := make([]int, len(key))
 	for i, r := range key {
 		if r < 'a' || r > 'z' {
 			return nil
 		}
-		offsets[i] = int(r - 'a')
+		distances[i] = int(r - 'a')
 	}
-	return vigenereCipher{key: key, offsets: offsets}
+	return vigenereCipher{distance: distances}
 }
 
 func (c vigenereCipher) Encode(s string) string {
 	runes := make([]rune, len(s))
-	i, n := 0, 0
+	i, dCount := 0, len(c.distance)
 	for _, r := range strings.ToLower(s) {
 		if r >= 'a' && r <= 'z' {
-			if n == len(c.offsets) {
-				n -= len(c.offsets)
-			}
-			offset := c.offsets[n]
-			runes[i] = runeShift(r, int(offset))
+			runes[i] = runeShift(r, c.distance[i%dCount])
 			i++
-			n++
 		}
 	}
 	return string(runes[0:i])
 }
 
 func (c vigenereCipher) Decode(s string) string {
-	decoder := newVigenereDecoder(c.key)
-	return decoder.Encode(s)
-}
-
-func newVigenereDecoder(encoderKey string) Cipher {
-	offsets := make([]int, len(encoderKey))
-	for i, r := range encoderKey {
-		offsets[i] = -1 * int(r-'a')
+	distances := make([]int, len(c.distance))
+	for i, v := range c.distance {
+		distances[i] = -1 * v
 	}
-	return vigenereCipher{key: encoderKey, offsets: offsets}
+	decoder := vigenereCipher{distance: distances}
+	return decoder.Encode(s)
 }
 
 func runeShift(r rune, shift int) rune {
