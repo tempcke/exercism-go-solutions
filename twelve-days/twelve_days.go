492a25798915d62a07c728c6c0017c0874b28a34
tempcke <t.github@logikostech.net>
2020-09-25 06:33:02 -0500

    twelve_days - changed scope of package level vars that are only used within a single function

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -5,37 +5,35 @@ import (
 	"strings"
 )
 
-const verseFormat = "On the %s day of Christmas my true love gave to me: %s."
-
-var gifts = []string{
-	"twelve Drummers Drumming, ",
-	"eleven Pipers Piping, ",
-	"ten Lords-a-Leaping, ",
-	"nine Ladies Dancing, ",
-	"eight Maids-a-Milking, ",
-	"seven Swans-a-Swimming, ",
-	"six Geese-a-Laying, ",
-	"five Gold Rings, ",
-	"four Calling Birds, ",
-	"three French Hens, ",
-	"two Turtle Doves, and ",
-	"a Partridge in a Pear Tree",
-}
-
-var days = []string{
-	"first", "second", "third", "fourth",
-	"fifth", "sixth", "seventh", "eighth",
-	"ninth", "tenth", "eleventh", "twelfth",
-}
-
 var verse []string
 
 // init builds each verse as each is constant
 func init() {
+	gifts := []string{
+		"twelve Drummers Drumming, ",
+		"eleven Pipers Piping, ",
+		"ten Lords-a-Leaping, ",
+		"nine Ladies Dancing, ",
+		"eight Maids-a-Milking, ",
+		"seven Swans-a-Swimming, ",
+		"six Geese-a-Laying, ",
+		"five Gold Rings, ",
+		"four Calling Birds, ",
+		"three French Hens, ",
+		"two Turtle Doves, and ",
+		"a Partridge in a Pear Tree",
+	}
+
+	days := []string{
+		"first", "second", "third", "fourth",
+		"fifth", "sixth", "seventh", "eighth",
+		"ninth", "tenth", "eleventh", "twelfth",
+	}
+
 	verse = make([]string, len(days))
 	for i := range days {
 		verse[i] = fmt.Sprintf(
-			verseFormat,
+			"On the %s day of Christmas my true love gave to me: %s.",
 			days[i],
 			strings.Join(gifts[11-i:], ""),
 		)
