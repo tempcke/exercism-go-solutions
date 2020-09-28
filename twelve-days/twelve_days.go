d7504db2ab67359374127e9a8cfa98bb991b8d5c
tempcke <t.github@logikostech.net>
2020-09-28 08:45:22 -0500

    twelve_days mentor thinks this way is prefered, I resisted it but finally gave in so we can move on, I prefer the previous version

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -1,43 +1,22 @@
 package twelve
 
 import (
-	"fmt"
 	"strings"
 )
 
-var verse []string
-
-// init builds each verse as each is constant
-func init() {
-	gifts := []string{
-		"twelve Drummers Drumming, ",
-		"eleven Pipers Piping, ",
-		"ten Lords-a-Leaping, ",
-		"nine Ladies Dancing, ",
-		"eight Maids-a-Milking, ",
-		"seven Swans-a-Swimming, ",
-		"six Geese-a-Laying, ",
-		"five Gold Rings, ",
-		"four Calling Birds, ",
-		"three French Hens, ",
-		"two Turtle Doves, and ",
-		"a Partridge in a Pear Tree",
-	}
-
-	days := []string{
-		"first", "second", "third", "fourth",
-		"fifth", "sixth", "seventh", "eighth",
-		"ninth", "tenth", "eleventh", "twelfth",
-	}
-
-	verse = make([]string, len(days))
-	for i := range days {
-		verse[i] = fmt.Sprintf(
-			"On the %s day of Christmas my true love gave to me: %s.",
-			days[i],
-			strings.Join(gifts[11-i:], ""),
-		)
-	}
+var verse = []string{
+	"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
+	"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
 }
 
 // Song returns the lyrics to 'The Twelve Days of Christmas'.
