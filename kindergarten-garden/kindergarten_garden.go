80b0e2257a99f85a631ef0d953cfa66ee8369742
tempcke <tempcke@gmail.com>
2021-02-08 08:37:49 -0600

    kindergarten_garden - create seperate builder object to keep Garden object as simple and small as possible

--- kindergarten-garden/kindergarten_garden.go
+++ kindergarten-garden/kindergarten_garden.go
@@ -7,11 +7,10 @@ import (
 	"strings"
 )
 
-// Garden represents the class garden
-type Garden struct {
-	children []string
-	plants   map[string][]byte
-}
+var (
+	rowCount         = 2
+	cupsPerKidPerRow = 2
+)
 
 var plant = map[byte]string{
 	'R': "radishes",
@@ -20,48 +19,94 @@ var plant = map[byte]string{
 	'V': "violets",
 }
 
+// Garden represents the class garden
+type Garden map[string][]string
+
 // NewGarden constructs and returns a Garden
 func NewGarden(diagram string, children []string) (*Garden, error) {
-	g := new(Garden)
-	g.children = append([]string{}, children...)
-	sort.Strings(g.children)
-
-	rows := strings.Split(diagram, "\n")
-	if len(rows) != 3 || len(rows[1]) != len(children)*2 {
-		return nil, errors.New("Wrong number of plants in diagram")
-	}
-	g.plants = make(map[string][]byte, len(children))
-
-	for i, name := range g.children {
-		if _, ok := g.plants[name]; ok {
-			return nil, fmt.Errorf("duplicate name: %s", name)
-		}
-		cups := []byte{
-			rows[1][i*2], rows[1][i*2+1],
-			rows[2][i*2], rows[2][i*2+1],
-		}
-		for _, c := range cups {
-			if _, ok := plant[c]; !ok {
-				return nil, fmt.Errorf("invalid cup code: %s", string(c))
-			}
-		}
-		g.plants[name] = cups
+	g := &gardenBuilder{
+		children: append([]string{}, children...),
+		rows:     strings.Split(diagram, "\n")[1:], // diagram must start with newline
 	}
 
-	return g, nil
+	garden, err := g.build()
+
+	return &garden, err
 }
 
 // Plants is used to query what plants each child has
-func (g *Garden) Plants(child string) ([]string, bool) {
-	bytes, ok := g.plants[child]
-	if !ok {
-		return nil, ok
-	}
-
-	plants := make([]string, len(bytes))
-	for i, b := range bytes {
-		plants[i] = plant[b]
-	}
-
+func (g Garden) Plants(child string) ([]string, bool) {
+	plants, ok := g[child]
 	return plants, ok
+}
+
+// gardenBuilder exists simply to share state among methods
+// while the Garden is being constructed and validated
+type gardenBuilder struct {
+	children []string
+	rows     []string
+}
+
+func (g *gardenBuilder) validate() error {
+	if err := g.validateChildren(); err != nil {
+		return err
+	}
+	return g.validateRows()
+}
+
+func (g *gardenBuilder) validateRows() error {
+	if len(g.rows) != rowCount {
+		return errors.New("Invalid Diagram")
+	}
+
+	// validate rows are the correct length
+	expectedLength := len(g.children) * cupsPerKidPerRow
+	for _, row := range g.rows {
+		if len(row) != expectedLength {
+			return errors.New("Wrong number of cups")
+		}
+
+		// validate each cup in each row is valid
+		for _, cup := range []byte(row) {
+			if _, ok := plant[cup]; !ok {
+				return fmt.Errorf("invalid cup code: %s", string(cup))
+			}
+		}
+	}
+	return nil
+}
+
+func (g *gardenBuilder) validateChildren() error {
+	var prev string
+	for _, name := range g.children {
+		if name == prev {
+			return errors.New("duplicate child names are not allowed")
+		}
+		prev = name
+	}
+	return nil
+}
+
+func (g *gardenBuilder) build() (Garden, error) {
+	sort.Strings(g.children)
+
+	if err := g.validate(); err != nil {
+		return nil, err
+	}
+	return g.getGarden(), nil
+}
+
+func (g *gardenBuilder) getGarden() Garden {
+	garden := make(map[string][]string, len(g.children))
+	for n, name := range g.children {
+		garden[name] = make([]string, 0, rowCount*cupsPerKidPerRow)
+
+		for _, row := range g.rows {
+			for j := 0; j < cupsPerKidPerRow; j++ {
+				cup := row[cupsPerKidPerRow*n+j]
+				garden[name] = append(garden[name], plant[cup])
+			}
+		}
+	}
+	return garden
 }
