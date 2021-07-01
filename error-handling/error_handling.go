bb6c37aa82d1a8ab611e10dce0db7a431688f3e6
Todd Empcke <tempcke@gmail.com>
2021-07-01 07:46:27 -0500

    error-handling solution 1

---
+++ error-handling/error_handling.go
@@ -0,0 +1,43 @@
+package erratum
+
+import "errors"
+
+// ErrFrobPanic is returned when the call to Resource.Frob causes a panic
+var ErrFrobPanic = errors.New("meh")
+
+// Use the resourceOpener to get a resource and Frob the input
+func Use(o ResourceOpener, input string) error {
+	var resource Resource
+	var err error
+
+	retryLimit := 10
+	for i := 0; i < retryLimit; i++ {
+		resource, err = o()
+		if err != nil {
+			if errors.As(err, &TransientError{}) {
+				continue
+			}
+			return err
+		}
+	}
+
+	defer resource.Close()
+	if ok := frob(resource, input); !ok {
+		return ErrFrobPanic
+	}
+	return nil
+}
+
+func frob(resource Resource, input string) bool {
+	defer frobPanicCheck(resource)
+	resource.Frob(input)
+	return true
+}
+
+func frobPanicCheck(resource Resource) {
+	if r := recover(); r != nil {
+		if err, ok := r.(FrobError); ok {
+			resource.Defrob(err.defrobTag)
+		}
+	}
+}
