c9d00bc3060849103ecf48870625626790bf1587
Todd Empcke <tempcke@gmail.com>
2021-07-09 06:28:33 -0500

    error-handling cleanup

--- error-handling/error_handling.go
+++ error-handling/error_handling.go
@@ -1,42 +1,48 @@
 package erratum
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
-// ErrFrobPanic is returned when the call to Resource.Frob causes a panic
-var ErrFrobPanic = errors.New("meh")
+// RetryLimit is used so we are confident the loop has an exit
+// by making it exported it allows the client to change the limit if needed
+// I don't like globals for this purpose but the challenge conditions didn't allow for anything else really
+var RetryLimit = 10
 
 // Use the resourceOpener to get a resource and Frob the input
-func Use(o ResourceOpener, input string) error {
-	var resource Resource
-	var err error
-
-	retryLimit := 10
-	for i := 0; i < retryLimit; i++ {
-		if resource, err = o(); err == nil {
-			break
-		}
-		if !errors.As(err, &TransientError{}) {
-			return err
-		}
+func Use(o ResourceOpener, input string) (err error) {
+	resource, err := openResource(o)
+	if err != nil {
+		return err
 	}
-
 	defer resource.Close()
-	if ok := frob(resource, input); !ok {
-		return ErrFrobPanic
-	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%v", r)
+			if e, ok := r.(FrobError); ok {
+				resource.Defrob(e.defrobTag)
+			}
+		}
+	}()
+
+	resource.Frob(input)
+
 	return nil
 }
 
-func frob(resource Resource, input string) bool {
-	defer frobPanicCheck(resource)
-	resource.Frob(input)
-	return true
-}
-
-func frobPanicCheck(resource Resource) {
-	if r := recover(); r != nil {
-		if err, ok := r.(FrobError); ok {
-			resource.Defrob(err.defrobTag)
+func openResource(o ResourceOpener) (r Resource, err error) {
+	for i := 0; ; i++ {
+		if r, err = o(); err == nil {
+			break
+		}
+		if !errors.As(err, &TransientError{}) {
+			return r, err
+		}
+		if i >= RetryLimit {
+			return r, errors.New("retry limit exceeded")
 		}
 	}
+	return r, nil
 }
