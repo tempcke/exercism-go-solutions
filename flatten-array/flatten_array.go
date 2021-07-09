3ce31254a9ed0693d4f8f7fa4199485ebfb7538e
Todd Empcke <tempcke@gmail.com>
2021-07-09 07:06:34 -0500

    flatten-array solved

--- flatten-array/flatten_array.go
+++ flatten-array/flatten_array.go
@@ -1,1 +1,21 @@
 package flatten
+
+// Flatten nested list into a flat list excluding nils
+func Flatten(in interface{}) []interface{} {
+	out := []interface{}{}
+
+	for _, val := range in.([]interface{}) {
+		switch v := val.(type) {
+		case []interface{}:
+			if v != nil {
+				out = append(out, Flatten(v)...)
+			}
+		default:
+			if v != nil {
+				out = append(out, v)
+			}
+		}
+	}
+
+	return out
+}
